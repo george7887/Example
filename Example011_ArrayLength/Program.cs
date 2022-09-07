8cf1bfb199193dc07d70b9ca85a066836a2631ec
george7887 <george788719@gmail.com>
2022-09-07 22:53:45 +0700

    Example011

---
+++ Example011_ArrayLength/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        Console.Write(col[position] + " ");
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int position = -1;
+    for ( int index = 0; index < count; index++)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+
+Console.WriteLine(pos);
+
