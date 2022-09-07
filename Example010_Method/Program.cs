b1ce62f14f424128ce2deb1e4f2743c81b3aa8f3
george7887 <george788719@gmail.com>
2022-09-07 22:52:26 +0700

    Example010

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//              0   1   2   3   4   5   6   7   8
+int[] array = {11, 211, 4, 41, 18, 15, 4, 17, 18, 19};
+int n = array.Length;
+int find = 18;
+for (int i = 0; i < n; i++)
+{
+    if (array[i] == find) {
+        Console.WriteLine(i);
+        break;
+    }
+}
